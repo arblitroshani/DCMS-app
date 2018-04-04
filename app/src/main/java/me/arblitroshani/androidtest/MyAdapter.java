24e48b65672d3f7e2a2c3126fe27e88c97159fb0
Arbli Troshani <arblitroshani@gmail.com>
2018-04-04 19:35:11 +0200

    jj

--- app/src/main/java/me/arblitroshani/androidtest/MyAdapter.java
+++ app/src/main/java/me/arblitroshani/androidtest/MyAdapter.java
@@ -1,7 +1,47 @@
 package me.arblitroshani.androidtest;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_service, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
